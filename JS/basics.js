db64cbd5b98ab1fc6afe3c4be5093b22e72dab77
= <=>
2023-06-30 11:15:19 +0100

    Introduction to variables

---
+++ JS/basics.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+let x = 1;
+ console.log( x+1 );
+ console.log( x*5 );
+
+ let y = "Hello World";
+
+ console.log ( y );
+
+ let srting = "Test";
+ let number = 9;
+ let animal = { animal: "cat", called: "Skye" };
+ 
+ console.log (animal);
+
+ let array = [1,2,3,4,5,6,7,8,9];
+ console.log(array);
+
+ let houseno = 15; //Local vairables
+ var street =" St Josephs Way"; // Global variables
+
+ console.log (houseno + street);
+
+ //.methodName() = method
+ //           {} = object
+ //           [] = array
---
+++ JS/exercises.js
@@ -0,0 +1,53 @@
+// Getting Started Page
+// Exercise 1
+
+let fname = "Naomi";
+let surname = "Stephenson";
+let home = "Swindon";
+let starsign = "Capricorn";
+
+let ex1ans = [fname,surname,home,starsign];
+
+console.log("Exercise 1 answers:", ex1ans);
+
+// Exercise 2
+
+let car = "Citreon";
+let model = "C3 Picasso Exclusive";
+
+console.log("Exercise 2 answers:","My favourite car is",car,"and the model is",model);
+
+// Exercise 3 - did not work!
+
+console.log("Exercise 3 answers:");
+console.log("%c Orange","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
+// Variables Page
+// Exercise 4 - exercise instructions are poor!
+
+let a;
+let b = "12345";
+let c =12344;
+let d = true;
+let e = {a:"Javascript"};
+
+console.log("Exercise 4 answers:");
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+// Exercise 5 - exercise instructions are poor!
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log("Exercise 5 answers:","The total bill is £",totalMoney,"the remaining amount of money to be paid is £",totalLeftToPay);
+
+// Exercise 6
+
+console.log("Exercise 6 answers:");
