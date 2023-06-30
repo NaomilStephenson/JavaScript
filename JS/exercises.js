7d056b955c9ef035408ee2df97c16d7041640cbc
= <=>
2023-06-30 14:22:14 +0100

    More Exercises competed

--- JS/exercises.js
+++ JS/exercises.js
@@ -1,53 +1,101 @@
 // Getting Started Page
 // Exercise 1
 
-let fname = "Naomi";
-let surname = "Stephenson";
-let home = "Swindon";
-let starsign = "Capricorn";
-
-let ex1ans = [fname,surname,home,starsign];
-
-console.log("Exercise 1 answers:", ex1ans);
+// let fname = "Naomi";
+// let surname = "Stephenson";
+// let home = "Swindon";
+// let starsign = "Capricorn";
+// let ex1ans = [fname,surname,home,starsign];
+// console.log("Exercise 1 answers:", ex1ans);
 
 // Exercise 2
 
-let car = "Citreon";
-let model = "C3 Picasso Exclusive";
-
-console.log("Exercise 2 answers:","My favourite car is",car,"and the model is",model);
+// let car = "Citreon";
+// let model = "C3 Picasso Exclusive";
+// console.log("Exercise 2 answers:","My favourite car is",car,"and the model is",model);
 
 // Exercise 3 - did not work!
 
-console.log("Exercise 3 answers:");
-console.log("%c Orange","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
-
+// console.log("Exercise 3 answers:");
+// console.log("%c Orange","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
 // Variables Page
 // Exercise 4 - exercise instructions are poor!
 
-let a;
-let b = "12345";
-let c =12344;
-let d = true;
-let e = {a:"Javascript"};
-
-console.log("Exercise 4 answers:");
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
-console.log(typeof(e));
+// let a;
+// let b = "12345";
+// let c =12344;
+// let d = true;
+// let e = {a:"Javascript"};
+// console.log("Exercise 4 answers:");
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
 
 // Exercise 5 - exercise instructions are poor!
 
-let totalMoney = 4000;
-let moneyPaidSoFar = 2348;
-let totalLeftToPay;
-
-totalLeftToPay = totalMoney - moneyPaidSoFar;
-
-console.log("Exercise 5 answers:","The total bill is £",totalMoney,"the remaining amount of money to be paid is £",totalLeftToPay);
+// let totalMoney = 4000;
+// let moneyPaidSoFar = 2348;
+// let totalLeftToPay;
+// totalLeftToPay = totalMoney - moneyPaidSoFar;
+// console.log("Exercise 5 answers:","The total bill is £",totalMoney,"the remaining amount of money to be paid is £",totalLeftToPay);
 
 // Exercise 6
 
-console.log("Exercise 6 answers:");
+// console.log("Exercise 6 answers:");
+// for (let A = 100; A <= 200; A++ ) {
+//     console.log(A);
+// }
+
+// Exercise 7
+
+// console.log("Exercise 7 answers:");
+// for (let A = 100; A <=200; A++) {
+//     if (A%2 == 0) {
+//             console.log(A,"-");
+//     }
+//     else {
+//             console.log(A,"*");
+//     }
+// }
+
+// Exercise 8
+
+// console.log("Exercise 8 answers:");
+// for (let N = 1; N <= 10; N++) {
+//     for (let M = 1; M <= 10; M++) {
+//         console.log(M);
+//     }
+// }
+
+// Exercise 9
+
+// console.log("Exercise 9 answers:");
+// let now = new Date();
+// let day = now.getDay();
+// switch (day) {
+//     case 0:
+//         console.log("Monday");
+//         break;
+//     case 1:
+//         console.log("Tuesday");
+//         break;
+//     case 2:
+//         console.log("Have a Break");
+//         break;
+//     case 3:
+//         console.log("Thursday");
+//         break;
+//     case 4:
+//         console.log("Friday");
+//         break;
+//     case 5:
+//         console.log("Saturday");
+//         break;
+//     case 6:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("I don't know!");
+// }
