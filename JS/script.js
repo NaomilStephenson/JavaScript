37aacda819441918d27e6ea0eb1e687282445e94
= <=>
2023-07-12 14:25:18 +0100

    Fizz buzz Challenge solved - whoop!

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+for (let n = 1; n <= 100; n++ ){
+    let testSet = [
+        {number: 3, answer: `Fizz`},
+        {number: 5, answer: `Buzz`},
+        {number: 7, answer: 'House'}
+    ];
+    let output = '';
+    let result = '';
+    for (let t of testSet){
+        (n % t.number) ? output = n : result += t.answer;
+    }
+    result ? console.log(result): console.log(output);
+};
